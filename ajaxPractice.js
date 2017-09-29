cef3f6bffc2c435fa6dca09d03bc0fd48f238203
Zachary Aten <z_man@Zacharys-MBP.net-worx.biz>
2017-09-29 16:14:34 -0500

    figured out how to append the image to the page

--- ajaxPractice.js
+++ ajaxPractice.js
@@ -6,7 +6,8 @@ $(document).ready(function () {
     $.get('https://dog.ceo/api/breeds/image/random', function(data){
       console.log(data.message);
       var randomDog = data.message;
-      
+      $('body').append('<img id="rdi" />');
+      $('#rdi').attr('src', randomDog);
     });
       /*$(".button").append("<img id='theDogImg'></img>");
       $('#theDogImg').attr('src', 'dogPicture');*/
