b628e86e4da51db925ec13aa5bce6ae0b0eb11a5
Zachary Aten <z_man@Zacharys-MacBook-Pro.local>
2017-09-29 07:48:07 -0500

    added jQuery .click functionality

---
+++ ajaxPractice.js
@@ -0,0 +1,16 @@
+/*
+$(function() {
+    $("#Doggo").click(function(){
+      $("#Doggo").text("Generating Doggo...");
+      console.log("Generating Doggo...");
+    })
+});
+*/
+
+$(document).ready(function () {
+  $(".Doggo").click(function(){
+    console.log("Generating Doggo...");
+    $(".Doggo").text("Generating Doggo...");
+    
+  })
+});
