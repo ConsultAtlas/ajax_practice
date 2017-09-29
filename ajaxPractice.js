d61f74d6380f84ff9c5ff6f58621bc287df2581a
Zachary Aten <z_man@Zacharys-MBP.net-worx.biz>
2017-09-29 15:43:29 -0500

    figured out how to get the url from the json object

--- ajaxPractice.js
+++ ajaxPractice.js
@@ -1,16 +1,15 @@
-/*
-$(function() {
-    $("#Doggo").click(function(){
-      $("#Doggo").text("Generating Doggo...");
-      console.log("Generating Doggo...");
-    })
-});
-*/
-
 $(document).ready(function () {
   $(".Doggo").click(function(){
     console.log("Generating Doggo...");
     $(".Doggo").text("Generating Doggo...");
-    $.get('https://dog.ceo/api/breeds/image/random');
-  })
+
+    $.get('https://dog.ceo/api/breeds/image/random', function(data){
+      console.log(data.message);
+      var randomDog = data.message;
+      
+    });
+      /*$(".button").append("<img id='theDogImg'></img>");
+      $('#theDogImg').attr('src', 'dogPicture');*/
+    });
+
 });
